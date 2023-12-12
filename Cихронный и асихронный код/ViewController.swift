4ced46c3002249ca9f555ed9404a8ea954a16542
AseinovSyimyk <aseinovsyimyk52@gmail.com>
2023-12-12 21:55:23 +0600

    Initial Commit

---
+++ Cихронный и асихронный код/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cихронный и асихронный код
+//
+//  Created by User on 12/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
